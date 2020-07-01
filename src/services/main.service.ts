dc044cfa575aa4e30bc248a022bc8f13fcd6269e
Alberto Ielpo <alberto.ielpo@ennova-research.com>
2020-07-01 18:34:43 +0200

    ++

--- src/services/main.service.ts
+++ src/services/main.service.ts
@@ -1,9 +1,14 @@
 import { Request, Response } from 'express';
 import ResponseModel from '../models/ResponseModel';
+import * as moment from 'moment-timezone';
 
 export class MainService {
   public version(req: Request, res: Response) {
     const pac = require('../../package.json');
+
+    const b = moment.tz.zonesForCountry('IT');
+    console.log(b);
+    console.log(b[0]);
     return ResponseModel.sendResponseOk(res, { version: pac.version });
   }
 }
